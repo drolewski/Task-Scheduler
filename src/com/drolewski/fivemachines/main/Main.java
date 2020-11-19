2dd49540ae53173d93399595ae98f900f4441959
rolex <drolewski@gmail.com>
2020-11-19 19:27:44 +0100

    Poprawka

--- src/com/drolewski/fivemachines/main/Main.java
+++ src/com/drolewski/fivemachines/main/Main.java
@@ -1,6 +1,7 @@
 package com.drolewski.fivemachines.main;
 
 import com.drolewski.fivemachines.algorithm.Algorithm;
+import com.drolewski.fivemachines.generator.Generator;
 import com.drolewski.fivemachines.verifier.InputData;
 import com.drolewski.fivemachines.verifier.OutputData;
 
@@ -13,18 +14,18 @@ import static com.drolewski.fivemachines.verifier.Verifier.verifySolution;
 public class Main {
     public static void main(String[] args) {
         // generate instances
-//        Generator.generateAllInstances();
+        Generator.generateAllInstances();
 
         // Read input instances
         List<InputData> jobsList = readInputData("src/com/drolewski/fivemachines/generator/generatedData");
 
         // Algorithm
-        for(InputData inputData : jobsList){
-            Algorithm algo = new Algorithm();
-            Map<Integer, List<Integer>> scheduledJobs = algo.algorithm(inputData);
-            // czasy
-            saveOutputFile(scheduledJobs, inputData);
-        }
+//        for(InputData inputData : jobsList){
+//            Algorithm algo = new Algorithm();
+//            Map<Integer, List<Integer>> scheduledJobs = algo.algorithm(inputData);
+//            // czasy
+//            saveOutputFile(scheduledJobs, inputData);
+//        }
 
         // Read output
         List<OutputData> outputData = readOutputData("src/com/drolewski/fivemachines/generator/generatedData");
